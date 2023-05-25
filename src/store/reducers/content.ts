dc411eff3a72464dcf81368bb7861c91b1953225
Альберт <albiqus@bk.ru>
2023-05-25 18:21:42 +0300

    MH-4: создать, подключить стор

---
+++ src/store/reducers/content.ts
@@ -0,0 +1,14 @@
+import { HOTELS } from "../../data/hotels"
+
+
+const startState = {
+    hotels: HOTELS,
+}
+
+export const content = (state = startState, action: any) => {
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+}
